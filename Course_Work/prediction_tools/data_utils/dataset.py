d54b860a5fb03da8a1b734d07a2983b9c839018f
Delev1n <nickhach23@gmail.com>
2023-03-19 02:07:34 +0400

    Added cross validation and dataloader creation

--- Course_Work/prediction_tools/data_utils/dataset.py
+++ Course_Work/prediction_tools/data_utils/dataset.py
@@ -3,9 +3,10 @@ from .ecg_2d_dataset import Ecg2dDataset
 from .ecg_1d_dataset import Ecg1dDataset
 from sklearn.preprocessing import MultiLabelBinarizer
 import ast
+from torch.utils.data import DataLoader
 
 
-def get_dataset(ecg_info: pd.DataFrame, cfg):
+def create_df(ecg_info: pd.DataFrame, cfg):
 
     ecg_info.reset_index(drop=True, inplace=True)
     one_hot = make_onehot(ecg_info, cfg.task_params.pathology_names)
@@ -13,7 +14,14 @@ def get_dataset(ecg_info: pd.DataFrame, cfg):
     if cfg.task_params.merge_map:
         one_hot = merge_columns(df=one_hot, merge_map=cfg.task_params.merge_map)
 
-    ecg_target = one_hot.values.tolist()
+    ecg_info["target"] = one_hot.values.tolist()
+
+    return ecg_info
+
+
+def get_dataloader(ecg_info: pd.DataFrame, cfg):
+
+    ecg_target = ecg_info["target"].to_list()
 
     if cfg.ecg_record_params.input_type == "2d":
         ecg_dataset = Ecg2dDataset(
@@ -23,8 +31,14 @@ def get_dataset(ecg_info: pd.DataFrame, cfg):
         ecg_dataset = Ecg1dDataset(
             ecg_info, ecg_target
         )
+    ecg_loader = DataLoader(
+        ecg_dataset,
+        batch_size=cfg.training_params.batch_size,
+        shuffle=True,
+        drop_last=True
+    )
 
-    return ecg_dataset
+    return ecg_loader
 
 
 def make_onehot(ecg_df, pathology_names):
--- Course_Work/prediction_tools/train.py
+++ Course_Work/prediction_tools/train.py
@@ -1,15 +1,27 @@
 import hydra
 from omegaconf import DictConfig
 import pandas as pd
-from data_utils.dataset import get_dataset
+from sklearn.model_selection import StratifiedKFold
+from data_utils.dataset import get_dataloader, create_df
 
 
 @hydra.main(version_base=None, config_path="../", config_name="config")
 def train(cfg: DictConfig):
 
-    df = pd.read_csv(cfg.train_df_path)
-    ecg_dataset = get_dataset(df, cfg)
-    print(ecg_dataset[0])
+    df = create_df(pd.read_csv(cfg.train_df_path), cfg)
+
+    skf = StratifiedKFold(n_splits=cfg.training_params.folds)
+    folds = cfg.training_params.folds
+
+    for fold_num, (train_index, val_index) in enumerate(
+            skf.split(
+                df, df["target"].apply(lambda x: "".join(str(it) for it in x))
+            )
+    ):
+        print("Fold number:", fold_num)
+        train_split_df, valid_split_df = df.iloc[train_index], df.iloc[val_index]
+        train_loader = get_dataloader(train_split_df, cfg)
+        valid_loader = get_dataloader(valid_split_df, cfg)
 
     return 0
 
